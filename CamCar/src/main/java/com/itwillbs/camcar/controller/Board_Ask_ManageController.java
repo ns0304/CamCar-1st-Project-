ef3b070f62c2b12b4f440320106767206ab5db54
OK <OK@DESKTOP-7LISS1K>
2024-07-12 21:10:53 +0900

    2024.07.12 옥혜지 수정본
    Run as 500 오류로
    Board_Ask_ManageController.java 내의
    내용 전체를 주석처리함(임시)

--- CamCar/src/main/java/com/itwillbs/camcar/controller/Board_Ask_ManageController.java
+++ CamCar/src/main/java/com/itwillbs/camcar/controller/Board_Ask_ManageController.java
@@ -1,13 +1,14 @@
-package com.itwillbs.camcar.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class Board_Ask_ManageController {
-
-	@GetMapping("board_ask")
-	public String member() {
-		return"board/board_ask";
-	}
-}
+//package com.itwillbs.camcar.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class Board_Ask_ManageController {
+//
+//	@GetMapping("board_ask")
+//	public String boardAskForm() {
+//		return "board/board_ask";
+//	}
+//
+//}
